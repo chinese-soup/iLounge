569eed6308e1e557458302e7e69209778eb58d39
chinese-soup <chinese-soup@users.noreply.github.com>
2023-11-05 02:58:44 +0100

    Initial Commit

---
+++ liff/liffApp.swift
@@ -0,0 +1,17 @@
+//
+//  liffApp.swift
+//  liff
+//
+//  Created by Jakub Mach on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct liffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
